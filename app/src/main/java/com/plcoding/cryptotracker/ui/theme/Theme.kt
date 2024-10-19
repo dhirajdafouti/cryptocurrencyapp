8b30d3bdc87047615f54c527958b4c97f0118ab1
CodingRock-Star <dhirajdafouti2020@gmail.com>
2024-10-19 21:57:29 +0530

    changes to files

--- app/src/main/java/com/plcoding/cryptotracker/ui/theme/Theme.kt
+++ app/src/main/java/com/plcoding/cryptotracker/ui/theme/Theme.kt
@@ -1,17 +1,14 @@
-package com.example.compose
-import android.app.Activity
+package com.plcoding.cryptotracker.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.Typography
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
@@ -255,7 +252,7 @@ val unspecified_scheme = ColorFamily(
 )
 
 @Composable
-fun AppTheme(
+fun CryptoTrackerTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
